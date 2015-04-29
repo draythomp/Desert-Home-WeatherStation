7a8b39d4b07b7d65627ae0ae37a16a1be9bd2548
David Thompson <noway@nowhere.com>
2015-04-29 14:28:36 -0700

    Setting up the Weather Station repository

---
+++ giveweather.py
@@ -0,0 +1,187 @@
+#! /usr/bin/python
+'''
+ This process waits for an HTTP request from someone wanting the
+ weather. It reads the data from a MySQL database on my NAS and formats it
+ into either JSON for a process interface or a very simple HTML page for
+ debugging or just checking around from time to time.
+'''
+import json
+from datetime import datetime, timedelta
+from houseutils import lprint, getHouseValues, timer, checkTimer, midnight
+from datetime import datetime, timedelta
+import sys
+import MySQLdb as mdb
+import argparse
+import cherrypy
+
+def collectWeather():
+    global Weather
+
+    dbconn = mdb.connect(host=dbHost, user=dbUser, passwd=dbPassword, db=dbName)
+    c = dbconn.cursor()
+    c.execute("select reading from ftemperature where utime = "
+            "(select max(utime) from ftemperature);")
+    Weather["currentOutsideTemp"] = c.fetchone()[0]
+    
+    c.execute("select reading from rtemperature where utime = "
+            "(select max(utime) from rtemperature);")
+    Weather["roofTemperature"] = c.fetchone()[0]
+    
+    c.execute("select speed from wind where utime = "
+            "(select max(utime) from wind);")
+    Weather["windSpeed"] = c.fetchone()[0]
+    
+    c.execute("select directionc from wind where utime = "
+            "(select max(utime) from wind);")
+    Weather["windDirectionC"] = c.fetchone()[0]
+    
+    c.execute("select directiond from wind where utime = "
+            "(select max(utime) from wind);")
+    Weather["windDirectionD"] = c.fetchone()[0]
+            
+    c.execute \
+        ("select reading from humidity where utime = "
+            "(select max(utime) from wind);")
+    Weather["humidity"] = c.fetchone()[0]
+    
+    c.execute("select reading from barometer where utime = "
+            "(select max(utime) from barometer);")
+    Weather["currentBarometric"] = c.fetchone()[0]
+    
+    # Get the cumulative readings
+    c.execute("select barometer from daily where utime = %s;",
+        (midnight(1),))
+    Weather["midnightBarometric"] = c.fetchone()[0]
+    
+    # now get the rest of the daily items
+    c.execute("select windhigh from daily where utime = %s;", \
+        (midnight(),))
+    Weather["maxWindSpeedToday"] = c.fetchone()[0]
+    
+    c.execute("select hightemp from daily where utime = %s;",\
+        (midnight(),))
+    Weather["maxTempToday"] = c.fetchone()[0]
+    
+    c.execute("select lowtemp from daily where utime = %s;",\
+        (midnight(),))
+    Weather["minTempToday"] = c.fetchone()[0]
+    
+    c.execute("select raincount from daily where utime = %s;",\
+        (midnight(1),))
+    startCount = c.fetchone()[0]
+    
+    c.execute("select reading  from raincounter where utime = "
+            "(select max(utime) from raincounter);")
+    endCount = c.fetchone()[0]
+    
+    Weather["rainToday"] = str((float(endCount) - float(startCount)) * 0.01)
+    dbconn.close()
+
+def showData():
+    global Weather
+    
+    returnString = ""
+    # fill in the Weather dictionary
+    collectWeather()
+    # now compose the screen
+    returnString += "Temperature on fence: " + Weather["currentOutsideTemp"] + \
+        "<br />"
+    returnString += "Temperature on roof: " + Weather["roofTemperature"] + \
+        "<br />"
+    returnString += "Wind Speed: " + Weather["windSpeed"] + "<br />"
+    returnString += "Wind Direction: " + Weather["windDirectionC"] + \
+        " (" + Weather["windDirectionD"] + ")" + "<br />"
+    returnString += "Humidity: " + Weather["humidity"] + "%" + "<br />"
+    returnString += "Barometer: " + Weather["currentBarometric"] + \
+        " mbar" + "<br />"
+    returnString += "<br />"
+    returnString += "Midnight Barometer Reading: " + Weather["midnightBarometric"] + \
+        " mbar" + "<br />"
+    returnString += "High Temperature Today: " + Weather["maxTempToday"] + \
+        "<br />"
+    returnString += "Low Temperature Today: " + Weather["minTempToday"] + \
+        "<br />"
+    returnString += "Highest Wind Speed Today: " + Weather["maxWindSpeedToday"] + \
+        "<br />"
+    returnString += "Rainfall Today: " + Weather["rainToday"] + \
+        " inch" + "<br />"
+    
+    return(returnString)
+
+def keepAlive():
+    # this is only to log that the process is still alive
+    lprint(" keep alive")
+
+# This is the process interface, it consists of a status report for humans
+# and a json output for other things.
+class WeatherSC(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_out() # This allows a dictionary input to go out as JSON
+    def status(self):
+        collectWeather()
+        return Weather
+        
+    @cherrypy.expose
+    def index(self):
+        status = "<strong>Desert Home Weather</strong><br /><br />"
+        status += showData()
+        return status
+        
+####################### Actually Starts Here ################################    
+debug = False
+Weather ={ 
+    "currentBarometric": "default",
+    "midnightBarometric":"default",
+    "rainToday": "default",
+    "currentOutsideTemp": "default",
+    "roofTemperature": "default",
+    "humidity": "default",
+    "windSpeed" : "default",
+    "windDirectionC" : "default",
+    "windDirectionD" : "default",
+    "maxWindSpeedToday" : "default",
+    "maxTempToday" : "default",
+    "minTempToday" : "default"
+    }
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug",
+        action = "store_true",
+        help='debug flag')
+    args = parser.parse_args()
+    if args.debug:
+        print "Running with debug on"
+        debug = True
+  
+    # the database where I'm storing stuff
+    hv = getHouseValues()
+    dbName = hv["weatherDatabase"]
+    dbHost = hv["weatherHost"]
+    dbPassword = hv["weatherPassword"]
+    dbUser = hv["weatherUser"]
+   # Get the ip address and port number you want to use
+    # from the houserc file
+    ipAddress=getHouseValues()["giveweather"]["ipAddress"]
+    port = getHouseValues()["giveweather"]["port"]
+    # periodically put a message into the log to indicate that
+    # this is still alive
+    keepAliveTimer = timer(keepAlive, minutes=4)
+  
+    cherrypy.config.update({'server.socket_host' : ipAddress.encode('ascii','ignore'),
+                            'server.socket_port': port,
+                            'engine.autoreload.on': False,
+                            })
+    # Subscribe to the 'main' channel in cherrypy with my timer
+    cherrypy.engine.subscribe("main", checkTimer.tick)
+    lprint ("Hanging on the wait for HTTP message")
+    # Now just hang on the HTTP server looking for something to 
+    # come in.  The cherrypy dispatcher will update the things that
+    # are subscribed which will update the timers so the light
+    # status gets recorded.
+    cherrypy.quickstart(WeatherSC())
+    
+    sys.exit("Told to shut down");
+    
+        
+        
---
+++ saveweather.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python
+# watch out for cr in line above
+'''
+This process waits for input from standard in and updates a MySQL database
+on my NAS. It relies on a modified rtl_433 specifically adapted to the 
+AcuRite 5n1 sensor. So, rtl_433 gets the data, transmits it, the pi picks it
+up using software defined radio the data gets passed to this process and 
+recorded in a database. This process has no interaction with a person.
+'''
+import sys
+import json
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+import urllib2
+import BaseHTTPServer
+import logging
+import MySQLdb as mdb
+from datetime import datetime, timedelta
+from houseutils import getHouseValues, lprint, dbTimeStamp, midnight
+from time import localtime, strftime
+
+def recordInDatabase():
+    #lprint("Recording weather data")
+    # First update the daily totals in the Weather dictionary
+    getDailys()
+    if ("default" in Weather.values()):
+        lprint("Weather data not ready yet")
+        lprint(Weather)
+        return
+    dbconn = mdb.connect(host=dbHost, user=dbUser, passwd=dbPassword, db=dbName)
+    c = dbconn.cursor()
+    try: 
+        c.execute("insert into rtemperature (reading, utime)"
+            "values(%s, %s);",
+            (Weather["roofTemperature"],dbTimeStamp()))
+        c.execute("insert into humidity (reading, utime)"
+            "values(%s, %s);",
+            (Weather["humidity"],dbTimeStamp()))
+        c.execute("insert into wind (speed, directionc, directiond, utime)"
+            "values(%s,%s,%s,%s);",
+            (Weather["windSpeed"],
+            Weather["windDirectionC"],
+            Weather["windDirectionD"],
+            dbTimeStamp()))
+        c.execute("insert into raincounter (reading, utime)"
+            "values(%s, %s);",
+            (Weather["rainCounter"],dbTimeStamp()))
+        dbconn.commit() # just in case the code below fails
+        # now adjust the daily mins and maxs
+        # This record gets updated all day and a new one gets created
+        # whenever a reading comes in after midnight. This means that
+        # the cumulative number are held under midnight of the previous
+        # day. This makes it a little confusing when calculating things
+        # like rainfall because you get the record for yesterday to 
+        # represent the last reading of yesterday. As in max(today) - 
+        # yesterday will give you today's rainfall.
+        c.execute('''INSERT INTO daily
+            (hightemp, lowtemp, windhigh, barometer, raincount, utime)
+            VALUES
+                (%s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                hightemp = values(hightemp),
+                lowtemp = values(lowtemp),
+                windhigh = values(windhigh),
+                barometer = values(barometer),
+                raincount = values(raincount); ''',
+            (Weather["maxTempToday"], 
+            Weather["minTempToday"], 
+            Weather["maxWindSpeedToday"], 
+            Weather["latestBarometric"],
+            Weather["rainCounter"],
+            midnight(),))
+        dbconn.commit()
+    except mdb.Error, e:
+        lprint ("Database Error %d: %s" % (e.args[0],e.args[1]))
+    dbconn.close()
+    #lprint("Finished recording items");
+
+def getDailys():
+    # get maxs, mins and such out of the database to pick
+    # up where we left off in case of failure and restart.
+    # Midnight is considered the first instant of today.
+    m = midnight()  # midnight today
+    n = dbTimeStamp()
+    dbconn = mdb.connect(host=dbHost, user=dbUser, passwd=dbPassword, db=dbName)
+    c = dbconn.cursor()
+    c.execute \
+        ("select min(reading/1.0) from ftemperature where utime between %s and %s;",
+        (m, n))
+    Weather["minTempToday"] = c.fetchone()[0]
+
+    c.execute \
+        ("select max(reading/1.0) from ftemperature where utime between %s and %s;",
+        (m, n))
+    Weather["maxTempToday"] = c.fetchone()[0]
+
+    c.execute \
+        ("select max(speed/1.0) from wind where utime between %s and %s;",
+        (m, n))
+    Weather["maxWindSpeedToday"] = c.fetchone()[0]
+    
+    c.execute \
+        ("SELECT reading FROM barometer ORDER BY utime DESC LIMIT 1;")
+    Weather["latestBarometric"] = c.fetchone()[0]
+    dbconn.close()
+    
+def midnightReset():
+    Weather["maxWindSpeedToday"] = Weather["windSpeed"]
+    Weather["maxTempToday"] = Weather["currentOutsideTemp"]
+    Weather["minTempToday"] = Weather["currentOutsideTemp"]
+    
+def recordInLog():
+    lprint(sys.argv[0]," Running")
+    lprint(Weather)
+
+
+direction={"NNW":337.5,"NW":315,"WNW":292.5,"W":270,
+        "WSW":247.5,"SW":225,"SSW":202.5,"S":180,
+        "SSE":157.5,"SE":135,"ESE":112.5,"E":90,
+        "ENE":67.5,"NE":45,"NNE":22.5,"N":0}
+
+Weather ={ 
+    #"currentBarometric": "default", The barometer is not read here
+    "latestBarometric":"default",
+    "rainCounter": "default",
+    #"currentOutsideTemp": "default", This is from the fence sensor also
+    "roofTemperature": "default",
+    "humidity": "default",
+    "windSpeed" : "default",
+    "windDirectionC" : "default",
+    "windDirectionD" : "default",
+    "maxWindSpeedToday" : "default",
+    "maxTempToday" : "default",
+    "minTempToday" : "default"
+    }
+#-------------------------------------------------  
+# get the values out of the houserc file
+hv = getHouseValues()
+dbName = hv["weatherDatabase"]
+dbHost = hv["weatherHost"]
+dbPassword = hv["weatherPassword"]
+dbUser = hv["weatherUser"]
+
+buff = ''
+data = ""
+char = ""
+#-------------------------------------------------
+logging.basicConfig()
+#------------------If you want to schedule something to happen -----
+scheditem = BackgroundScheduler()
+# This resets the daily items at midnight; actually, just a few
+# seconds before midnight to avoid problems with overshoot
+scheditem.add_job(midnightReset, 'cron', hour=23, minute=59, second=50)
+
+# the "I'm alive right now" message
+scheditem.add_job(recordInLog, 'interval', minutes=30)
+# Update the database periodically
+scheditem.add_job(recordInDatabase, 'interval', seconds=60)
+scheditem.start()
+
+recordInLog()
+#get daily cumulative numbers
+getDailys()
+print "initial readings", Weather;
+'''
+ Now just hang up in a loop reading the radio output.
+ The data is held and updated in the Weather dictionary with database
+ updates to record it on minute intervals handled by the timer above.
+'''
+while True:
+    try:
+        char = sys.stdin.read(1) #This is a blocking read,
+        # (you have no idea how hard it was to discover this)
+        # an end of file on a piped in input is a length of 
+        # zero.  There's about a thousand wrong answers out there
+        # and I never did find the right one.  Thank goodness
+        # for good old trial and error.
+        if len(char) == 0:
+            break # the pipe is gone, just exit the process
+        else:
+            buff += char;
+        if buff.endswith('\n'):
+            try:
+                data = json.loads(buff[:-1])
+            except ValueError:
+                lprint("Input from receive wasn't JSON");
+                lprint(buff);
+                buff = ''
+                continue
+            # Now, fill in the Weather dictionary for use by
+            # everything else
+            # beginning with the five readings from the sensor head
+            Weather["windSpeed"] = data["windSpeed"]["WS"]
+            Weather["windDirectionC"] = data["windDirection"]["WD"]
+            Weather["roofTemperature"] = data["temperature"]["T"]
+            Weather["humidity"] = data["humidity"]["H"]
+            Weather["rainCounter"] = data["rainCounter"]["RC"]
+            if (Weather["windDirectionC"] in direction):
+                Weather["windDirectionD"] = direction[Weather["windDirectionC"]]
+            # Now update max wind speed (temps are taken from another sensor)
+            if (Weather["maxWindSpeedToday"] == "default"):
+                Weather["maxWindSpeedToday"] = data["windSpeed"]["WS"]
+            if(float(Weather["maxWindSpeedToday"]) < float(data["windSpeed"]["WS"])):
+                Weather["maxWindSpeedToday"] = data["windSpeed"]["WS"]
+            # and start the buffer over from scratch
+            buff = ''
+
+    except KeyboardInterrupt:
+        lprint("Cntl-C from user");
+        break;
+              
+scheditem.shutdown(wait=False)
+lprint(sys.argv[0],"Done")
+sys.stdout.flush()
+sys.exit("")
